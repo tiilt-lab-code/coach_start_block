c25e2759d6c26c135c5db1b6d12739500634fbe0
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-06-04 06:10:36 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,20 +7,21 @@ input.onButtonPressed(Button.AB, function () {
 })
 radio.onReceivedString(function (receivedString) {
     c_index = list.indexOf(radio.receivedPacket(RadioPacketProperty.SerialNumber))
+    c_time = control.millis()
     if (receivedString == "pair") {
         if (list.length >= 8) {
             radio.sendString("full")
         } else {
             if (c_index < 0) {
                 list.push(radio.receivedPacket(RadioPacketProperty.SerialNumber))
-                c_index = list.indexOf(radio.receivedPacket(RadioPacketProperty.SerialNumber))
             }
             radio.sendValue(convertToText(c_index + 1), radio.receivedPacket(RadioPacketProperty.SerialNumber))
         }
     } else if (receivedString == "movement") {
-        datalogger.log(datalogger.createCV("lane", convertToText(c_index + 1)), datalogger.createCV("time", radio.receivedPacket(RadioPacketProperty.Time)))
+        datalogger.log(datalogger.createCV("lane", convertToText(c_index + 1)), datalogger.createCV("time", c_time - start_time))
     }
 })
+let c_time = 0
 let c_index = 0
 let start_time = 0
 let list: number[] = []
